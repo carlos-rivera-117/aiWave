7383004e1a5b0cfefd185dc7a02f02ac810974c9
Carlos Rivera <carlosrc1995@gmail.com>
2024-06-07 01:29:30 -0400

    (services): Add Generative AI Services Section
    Implemented the Services component to showcase the features of Brainwave's generative AI capabilities. The component includes:
    - A section with a heading and descriptive text.
    - Three main service sections: Smartest AI, Photo Editing, and Video Generation, each with detailed descriptions and visual elements.
    - Lists of service benefits, each with an icon.
    - Call-to-action elements for enhanced user engagement.
    
    Added imports for necessary assets and constants and utilized existing design components for consistency.
    Feat #000007 Services Section

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import PropTypes from "prop-types";
 
-const Heading = ({ className, title }) => {
+const Heading = ({ className, title, text }) => {
   return (
     <div className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20`}>
       {title && <h2 className="h2">{title}</h2>}
+      {text && <p className="body-2 mt-4 text-n-4">{text}</p>}
     </div>
   );
+};
+
+Heading.propTypes = {
+  className: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
 };
 
 export default Heading;
